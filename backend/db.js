1d0b454a4c0557be171bd386ccc5633e74aeb0d6
Kim Nguyen <Kim Nguyen>
2025-10-08 14:12:29 +0200

    prepare database with 3 users (test)

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,17 @@
-const Database = require("better-sqlite3");
+const sqlite = require("better-sqlite3");
 
-// crée (ou ouvre) un fichier database.sqlite
-const db = new Database("/data/database.sqlite");
+// crée ou ouvre le fichier database.sqlite
+const db = new sqlite.db("/data/database.sqlite");
 
 // crée la table si elle n'existe pas
-db.prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, score INTEGER)").run();
+db.prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, email TEXT, score INTEGER)").run();
 
 // insère une donnée
-db.prepare("INSERT INTO users (username, score) VALUES (?, ?)").run("bob", 100);
+db.prepare("INSERT INTO users (username, email, score) VALUES (?, ?, ?)").run("kimnguye", "kimnguye@42.fr", 0);
+db.prepare("INSERT INTO users (username, email, score) VALUES (?, ?, ?)").run("kduroux", "kduroux@42.fr", 0);
+db.prepare("INSERT INTO users (username, email, score) VALUES (?, ?, ?)").run("hgirard", "hgirard@42.fr", 0);
 
-// lit les données
+// lit les données et les affiche sur la console
 const rows = db.prepare("SELECT * FROM users").all();
 console.log(rows);
 
