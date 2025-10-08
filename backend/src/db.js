c726b5455093b5e5409db19c9add9d3a0ad10cbd
kimnguye <kimnguye@42.fr>
2025-10-08 20:46:57 +0000

    gestion valeur par defaut

--- backend/src/db.js
+++ backend/src/db.js
@@ -6,16 +6,17 @@ import 'dotenv/config';
 const db = new Database("/data/database.db");
 
 // crée la table si elle n'existe pas
-db.prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, email TEXT UNIQUE, password TEXT, score INTEGER)").run();
+// on refuse les doublons de username et de email
+db.prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, email TEXT UNIQUE, password TEXT, photo TEXT DEFAULT NULL, wins INTEGER DEFAULT 0, losses INTEGER DEFAULT 0)").run();
 
 // insère une donnée
 try{
     const hashed1 = await bcrypt.hash(process.env.MDP1, 10);
     const hashed2 = await bcrypt.hash(process.env.MDP2, 10);
     const hashed3 = await bcrypt.hash(process.env.MDP3, 10);
-    db.prepare("INSERT INTO users (username, email, password, score) VALUES (?, ?, ?, ?)").run("kimnguye", "kimnguye@42.fr", hashed1, 0);
-    db.prepare("INSERT INTO users (username, email, password, score) VALUES (?, ?, ?, ?)").run("kduroux", "kduroux@42.fr", hashed2, 0);
-    db.prepare("INSERT INTO users (username, email, password, score) VALUES (?, ?, ?, ?)").run("hgirard", "hgirard@42.fr", hashed3, 0);
+    db.prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)").run("kimnguye", "kimnguye@42.fr", hashed1);
+    db.prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)").run("kduroux", "kduroux@42.fr", hashed2);
+    db.prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)").run("hgirard", "hgirard@42.fr", hashed3);
 } catch (err) {
     if (err.code == "SQLITE_CONSTRAINT_UNIQUE"){
         console.log("Username or email already exists");
