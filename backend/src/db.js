212c36e27142ac22cfbb3640627c0007bcaaed32
kimnguye <kimnguye@42.fr>
2025-10-08 18:51:50 +0000

    hashed passwords

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,4 +1,6 @@
 import Database from "better-sqlite3";
+import bcrypt from "bcrypt";
+import 'dotenv/config';
 
 // crée ou ouvre le fichier database.db
 const db = new Database("/data/database.db");
@@ -8,9 +10,12 @@ db.prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username T
 
 // insère une donnée
 try{
-    db.prepare("INSERT INTO users (username, email, password, score) VALUES (?, ?, ?, ?)").run("kimnguye", "kimnguye@42.fr", "mdp1", 0);
-    db.prepare("INSERT INTO users (username, email, password, score) VALUES (?, ?, ?, ?)").run("kduroux", "kduroux@42.fr", "mdp2", 0);
-    db.prepare("INSERT INTO users (username, email, password, score) VALUES (?, ?, ?, ?)").run("hgirard", "hgirard@42.fr", "mdp3", 0);
+    const hashed1 = await bcrypt.hash(process.env.MDP1, 10);
+    const hashed2 = await bcrypt.hash(process.env.MDP2, 10);
+    const hashed3 = await bcrypt.hash(process.env.MDP3, 10);
+    db.prepare("INSERT INTO users (username, email, password, score) VALUES (?, ?, ?, ?)").run("kimnguye", "kimnguye@42.fr", hashed1, 0);
+    db.prepare("INSERT INTO users (username, email, password, score) VALUES (?, ?, ?, ?)").run("kduroux", "kduroux@42.fr", hashed2, 0);
+    db.prepare("INSERT INTO users (username, email, password, score) VALUES (?, ?, ?, ?)").run("hgirard", "hgirard@42.fr", hashed3, 0);
 } catch (err) {
     if (err.code == "SQLITE_CONSTRAINT_UNIQUE"){
         console.log("Username or email already exists");
