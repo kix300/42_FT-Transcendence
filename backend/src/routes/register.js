939ca689910d83883624b0ef254848b33bc0f60d
Kim Nguyen <kimnguye@f2r5s6.clusters.42paris.fr>
2025-10-13 12:05:12 +0200

    fix RegisterPage.ts to always send Multipart Form

--- backend/src/routes/register.js
+++ backend/src/routes/register.js
@@ -21,6 +21,7 @@ export default async function registerRoutes(fastify ){
 				if (part.fieldname === "password") password = part.value;
 			}
 		}
+		console.log('✅ On va enregistrer un nouveau user: ', username);
 		if (avatar)
 		{
 			const uploadsDir = path.join(process.cwd(), 'uploads');
@@ -51,3 +52,34 @@ export default async function registerRoutes(fastify ){
         }
     });
 }
+
+
+
+
+// import bcrypt from "bcrypt";
+// import db from "../db.js";
+
+// export default async function registerRoutes(fastify ){
+//     fastify.post("/api/register", async(request, reply) => {
+//         const {username, email, password, photo} = request.body;
+
+//         try{
+//             const hashedPassword = await bcrypt.hash(password, 10);
+// 			if (photo){
+//             	db.prepare('INSERT INTO users (username, email, password, photo) VALUES (?, ?, ?, ?)').run(username, email, hashedPassword, photo);
+// 			} else {
+
+// 				db.prepare('INSERT INTO users (username, email, password) VALUES (?, ?, ?)').run(username, email, hashedPassword);
+// 			}
+//             reply.code(201).send({message:"User created successfully"});
+//         } catch (err) {
+//             if (err.code == "SQLITE_CONSTRAINT_UNIQUE"){
+//                 reply.code(400).send({error: "Username or email already exists"});
+//             }
+//             else {
+//                 console.error(err);
+//                 reply.code(500).send({error: "Internal server error"});
+//             }
+//         }
+//     });
+// }
