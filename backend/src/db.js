052ea6e431757cb7eb4ce2c3a13e133b023e3129
Kim Nguyen <kimnguye@f2r4s4.clusters.42paris.fr>
2025-10-13 18:17:20 +0200

    table matches

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,7 +5,16 @@ import 'dotenv/config';
 // crée ou ouvre le fichier database.db
 const db = new Database("/data/database.db");
 
-// crée la table si elle n'existe pas
+
+
+/*****************************************************************/ 
+/*                                                               */ 
+/*                 TABLE USERS                                   */ 
+/*                                                               */ 
+/*****************************************************************/ 
+
+
+// crée la table users si elle n'existe pas
 // on refuse les doublons de username et de email
 db.prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, email TEXT UNIQUE, password TEXT, photo TEXT DEFAULT './uploads/avatar.png', wins INTEGER DEFAULT 0, losses INTEGER DEFAULT 0)").run();
 
@@ -50,5 +59,55 @@ db.prepare(`UPDATE users SET role = 'user' WHERE role IS NULL`).run();
 // lit les données et les affiche sur la console
 const rows = db.prepare("SELECT * FROM users").all();
 console.log(rows);
+
+
+
+/*****************************************************************/ 
+/*                                                               */ 
+/*                 TABLE MATCHES                                 */ 
+/*                                                               */ 
+/*****************************************************************/ 
+
+
+// crée la table si elle n'existe pas
+db.prepare(`CREATE TABLE IF NOT EXISTS matches (
+	id INTEGER PRIMARY KEY,
+	player1_id INTEGER NOT NULL,
+	player2_id INTEGER NOT NULL,
+	winner_id INTEGER NOT NULL,
+	player1_score INTEGER,
+	player2_score INTEGER,
+	is_tournament BOOLEAN DEFAULT 0, --0 = false, 1 = true
+	date DATETIME TEXT DEFAULT (datetime('now', 'localtime')),
+	FOREIGN KEY (player1_id) REFERENCES users(id),
+	FOREIGN KEY (player2_id) REFERENCES users(id),
+	FOREIGN KEY (winner_id) REFERENCES users(id)
+	)`).run();
+
+
+
+/*inserer une nouvelle stat*/
+db.prepare(`
+  INSERT INTO matches (player1_id, player2_id, winner_id, score, is_tournament)
+  VALUES (?, ?, ?, ?, ?)
+`).run(player1_id, player2_id, winner_id, "3-2", true);
+
+
+/*recuperer lhistorique dun joueur*/
+const matches = db.prepare(`
+  SELECT m.*, 
+         u1.username AS player1_name,
+         u2.username AS player2_name,
+         uw.username AS winner_name
+  FROM matches m
+  JOIN users u1 ON m.player1_id = u1.id
+  JOIN users u2 ON m.player2_id = u2.id
+  JOIN users uw ON m.winner_id = uw.id
+  WHERE m.player1_id = ? OR m.player2_id = ?
+  ORDER BY m.played_at DESC
+`).all(userId, userId);
+
+
+
 
 export default db;
