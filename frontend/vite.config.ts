c14db3d8a46699cf33d31c20f66f9224b39b8a35
kix300 <kixwalkiki@gmail.com>
2025-10-16 17:31:42 +0200

    Fix(docker-front): npm run build --watch

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  root: './',
+  plugins: [tailwindcss()],
+  root: "./",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
-})
+});
